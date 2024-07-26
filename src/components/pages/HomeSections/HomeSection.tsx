bba1b6d300ed3d2378ee3e57fea035d0aafeedf5
Bekzattdev <alpberlinfox@gmail.com>
2024-07-26 22:39:10 +0600

    chek

--- src/components/pages/HomeSections/HomeSection.tsx
+++ src/components/pages/HomeSections/HomeSection.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react'
-import scss from './HomeSection.module.scss'
-import { useGetRatingQuery } from '@/redux/api/infomovie'
-import { useGetMeQuery } from '@/redux/api/auth'
-import { useGetMovieQuery } from '@/redux/api/movie'
+import { useEffect, useState } from "react";
+import scss from "./HomeSection.module.scss";
+import { useGetRatingQuery } from "@/redux/api/infomovie";
+import { useGetMeQuery } from "@/redux/api/auth";
+import { useGetMovieQuery } from "@/redux/api/movie";
 
 const HomeSection = () => {
-  const [listFilm, setListFilm] = useState<MOVIE.GetMovieResponse>()
+  const [listFilm, setListFilm] = useState<MOVIE.GetMovieResponse>();
 
-  const {data, isSuccess}  = useGetMovieQuery()
+  const { data, isSuccess } = useGetMovieQuery();
   useEffect(() => {
     if (data) {
-      setListFilm(data)
+      setListFilm(data);
     }
-  }, [isSuccess])
+  }, [isSuccess]);
   return (
     <section className={scss.HomeSection}>
       <div className="container">
@@ -20,14 +20,12 @@ const HomeSection = () => {
           <h1>HomeSection</h1>
           <div className={scss.list}>
             <img src={data?.image} alt="" />
-            <h1>
-              {data?.title}
-            </h1>
+            <h1>{data?.title}</h1>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HomeSection
+export default HomeSection;
